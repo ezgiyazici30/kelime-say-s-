8f88b7cc65aaa915b36c23baf15f5c2049a4b943
ezgiyazici30 <ezgiyazici3011@gmail.com>
2024-11-21 15:28:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Bir cümle girin:");
+        string cumle = Console.ReadLine();
+
+       
+        string[] kelimeler = cumle.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        
+        Console.WriteLine(string.Format("Cümledeki kelime sayısı: {0}", kelimeler.Length));
+    }
+}
+
